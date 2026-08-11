ea222057bec12e88ac3ee74059e61a1344cbcbdd
Anass Jabrane <jabraneanass12@gmail.com>
2026-08-11 16:09:05 +0100

    Add Booking model relationships

--- bde-events-api/app/Models/Booking.php
+++ bde-events-api/app/Models/Booking.php
@@ -11,6 +11,14 @@ class Booking extends Model
         'user_id',
         'event_id',
     ];
-    
+     public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
 }
 
