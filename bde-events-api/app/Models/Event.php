80d78eeef84799b9d04449b5333b6dbf13832ae6
Anass Jabrane <jabraneanass12@gmail.com>
2026-08-11 11:50:57 +0100

    Create Event model

---
+++ bde-events-api/app/Models/Event.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'date',
+        'time',
+        'location',
+        'price',
+        'max_capacity',
+    ];
+}
