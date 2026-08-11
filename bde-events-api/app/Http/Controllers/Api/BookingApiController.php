3ff057bd60f68b1e1195503995322ad18a753241
Anass Jabrane <jabraneanass12@gmail.com>
2026-08-11 16:45:32 +0100

    Add booking creation API

--- bde-events-api/app/Http/Controllers/Api/BookingApiController.php
+++ bde-events-api/app/Http/Controllers/Api/BookingApiController.php
@@ -3,9 +3,30 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Booking;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class BookingApiController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $event = Event::find($request->event_id);
+
+        if (!$event) {
+            return response()->json([
+                'message' => 'Event not found'
+            ], 404);
+        }
+
+        $booking = Booking::create([
+            'user_id' => $request->user_id,
+            'event_id' => $event->id,
+        ]);
+
+        return response()->json([
+            'message' => 'Booking created successfully',
+            'booking' => $booking
+        ], 201);
+    }
 }
