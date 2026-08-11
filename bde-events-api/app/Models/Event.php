525709509e1159b3149f811be92d6a461a131a94
Anass Jabrane <jabraneanass12@gmail.com>
2026-08-11 16:35:31 +0100

    Add bookings relationship to Event model

--- bde-events-api/app/Models/Event.php
+++ bde-events-api/app/Models/Event.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
@@ -15,4 +16,8 @@ class Event extends Model
         'price',
         'max_capacity',
     ];
+    public function bookings(): HasMany
+{
+    return $this->hasMany(Booking::class);
+}
 }
